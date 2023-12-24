c1dec54e0c8670e645f2374affc3c87fc97de90b
sakhawat71 <sakhawat.showrav@gmail.com>
2023-12-24 20:04:07 +0600

    firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuAerQj0BUR0x42k5C2Dqlx4b1MDbvC0w",
+  authDomain: "online-group-study-71.firebaseapp.com",
+  projectId: "online-group-study-71",
+  storageBucket: "online-group-study-71.appspot.com",
+  messagingSenderId: "775176610538",
+  appId: "1:775176610538:web:c688f477f0a7e939ac6d93"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
