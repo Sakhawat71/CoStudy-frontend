fb722076d36d5875a03dbc2de4271d44b2f0197c
sakhawat71 <sakhawat.showrav@gmail.com>
2024-01-23 23:44:02 +0600

    QnA complete

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -6,9 +6,9 @@ const Banner = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <Link to={"/assignments"}><button className="btn btn-ghost hover:text-blue-700">All Assignments</button></Link>
+                    <h1 className="mb-5 text-5xl font-bold">CoStudy</h1>
+                    <p className="mb-5 text-xl"> Learn Together,<br /> Succeed Together </p>
+                    <Link to={"/assignments"}><button className="btn btn-ghost hover:text-blue-700">Join the Community!</button></Link>
                 </div>
             </div>
         </div>
