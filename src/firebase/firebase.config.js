a8977e46ffb30525d575cede68288f8c52be4c3d
sakhawat71 <sakhawat.showrav@gmail.com>
2024-01-24 21:58:53 +0600

    firebase + env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,18 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCuAerQj0BUR0x42k5C2Dqlx4b1MDbvC0w",
-  authDomain: "online-group-study-71.firebaseapp.com",
-  projectId: "online-group-study-71",
-  storageBucket: "online-group-study-71.appspot.com",
-  messagingSenderId: "775176610538",
-  appId: "1:775176610538:web:c688f477f0a7e939ac6d93"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+const auth = getAuth(app)
+export default auth;
